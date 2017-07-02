3b081fa3bffb8dab89992cc21677a4adb31e3c3a
Jerry Gao <gao@blackwright.com>
2017-07-02 10:56:35 -0400

    remove ineffective selector

--- src/containers/ContentDisplayContainer.js
+++ src/containers/ContentDisplayContainer.js
@@ -1,24 +1,6 @@
 import { connect } from 'react-redux';
 import ContentDisplay from '../components/ContentDisplay';
 import Cartesian3 from 'cesium/Source/Core/Cartesian3';
-import { createSelector } from 'reselect';
-
-const getCameraLocation = state => state.camera.location;
-const getDotsList = state => state.dotsList.data;
-const getRadius = (state, ownProps) => ownProps.scene.camera.positionCartographic.height / 10;
-
-const selectComputedData = createSelector(
-  [getCameraLocation, getDotsList, getRadius],
-  (cameraLocation, dotsList, radius) => {
-    if (!cameraLocation || !dotsList) {
-      return [];
-    }
-    return dotsList.filter(dot => {
-      const dotLocation = Cartesian3.fromDegrees(dot.lng, dot.lat);
-      return distanceBetween(cameraLocation, dotLocation) < radius;
-    });
-  }
-)
 
 function distanceBetween(location1, location2) {
   return Cartesian3.distance(location1, location2);
@@ -35,14 +17,15 @@ function nearbyContent(cameraLocation, dotsList, radius) {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  // const cameraLocation = state.camera.location;
-  // const dotsList = state.dotsList.data;
-  // const { scene } = ownProps;
-  //
-  // const height = scene.camera.positionCartographic.height;
-  // const radius = height / 10;
+  const cameraLocation = state.camera.location;
+  const dotsList = state.dotsList.data;
+  const { scene } = ownProps;
+
+  const height = scene.camera.positionCartographic.height;
+  const radius = height / 10;
+
   return {
-    nearbyContent: selectComputedData(state, ownProps)
+    nearbyContent: nearbyContent(cameraLocation, dotsList, radius)
   };
 }
 
